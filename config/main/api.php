34cb55f7f37f4c864f02c213012e4182f1280bf4
lht <479923197@qq.com>
2024-12-04 19:03:24 +0800

    启用swoole

--- config/main/api.php
+++ config/main/api.php
@@ -1,13 +1,38 @@
 <?php
 
-return array(
+$configWindows = [
     'server_mode' => 'Fastcgi',
-    'project' => array(
+    'project' => [
         'log_level' => 2, //0上线模式，1错误显示+release，2错误显示+debug+release
         'view_mode' => 'Json',
         'headers' => [
             'Access-Control-Allow-Origin' => '*',
             'Content-Type' => 'application/json',
         ],
-    ),
-);
+    ],
+];
+$configLinux = [
+    'server_mode' => 'SwooleHttp',
+    'swoole_main' => [
+        'host' => '0.0.0.0',
+        'port' => 8005,
+        'setting' => [
+            'enable_coroutine' => true,
+            'max_coroutine' => 10000,
+            'worker_num' => 1,                              //工作进程数
+            'pid_file' => ROOT_PATH.'/api.pid',			//保存master进程id
+            'log_level' => 0,								//swoole日志级别（0DEBUG，1TRACE，2INFO，3NOTICE，4WARNING，5ERROR）
+            'max_request' => 80000,                         //单个进程处理请求次数
+            'enable_static_handler' => true
+        ],
+        'project' => [
+            'log_level' => 2, //0上线模式，1错误显示+release，2错误显示+debug+release
+            'view_mode' => 'Json',
+            'headers' => [
+                'Access-Control-Allow-Origin' => '*',
+                'Content-Type' => 'application/json',
+            ],
+        ],
+    ]
+];
+return IS_WIN ? $configWindows : $configLinux;
--- config/main/site.php
+++ config/main/site.php
@@ -1,12 +1,12 @@
 <?php
 
-$configWindows = array(
+$configWindows = [
     'server_mode' => 'Fastcgi',
-    'project' => array(
+    'project' => [
         'log_level' => 2, //0上线模式，1错误显示+release，2错误显示+debug+release
         'view_mode' => 'Template',
         'route_cmd' => \app_main\config\router::Cmd_Enum,
-    ),
+    ],
     //中间件
     'midware' => [
         'AccessLog' =>['key'=>['ctrl', 'method', 'time', 'uid', 'arg']],
@@ -15,29 +15,25 @@ $configWindows = array(
     'cache' => [
         'global'=>['_adapter'=>'Yac', 'prefix'=>'site'],
     ],
-);
+];
 
 
-$configLinux = array(
+$configLinux = [
     'server_mode' => 'SwooleHttp',
-    'swoole_main' => array(
+    'swoole_main' => [
         'host' => '0.0.0.0',
         'port' => 8005,
         'setting' => [
             'enable_coroutine' => true,
             'max_coroutine' => 10000,
             'worker_num' => 1,                              //工作进程数
-            'pid_file' => ROOT_PATH.'/http.pid',			//保存master进程id
+            'pid_file' => ROOT_PATH.'/site.pid',			//保存master进程id
             'log_level' => 0,								//swoole日志级别（0DEBUG，1TRACE，2INFO，3NOTICE，4WARNING，5ERROR）
             'max_request' => 80000,                         //单个进程处理请求次数
             'document_root' => ROOT_PATH,
             'enable_static_handler' => true,
-
-            'open_length_check' => true,
-            'package_length_type' => 'n',
-            'package_body_offset' => 2,
         ],
-    ),
+    ],
     'swoole_rpc' => [
         'host' => '0.0.0.0',
         'port' => 8006,
@@ -56,6 +52,6 @@ $configLinux = array(
     'cache' => [
         'global'=>['_adapter'=>'Yac', 'prefix'=>'site'],
     ],
-);
+];
 
 return IS_WIN ? $configWindows : $configLinux;
